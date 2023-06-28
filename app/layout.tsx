48afd61eab6d312efcd4487d428ce69173523260
Facundo N Godoy <facu14_23@hotmail.com>
2023-06-27 22:39:20 -0300

    NavBar & navigation

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import NavBar from "./components/navbar/NavBar";
+import "./globals.css";
+import { Nunito } from "next/font/google";
+
+export const metadata = {
+  title: "Airbnb",
+  description: "Airbnb Clone",
+};
+
+const font = Nunito({
+  subsets: ["latin"],
+});
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <NavBar />
+        {children}
+      </body>
+    </html>
+  );
+}
