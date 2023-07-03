c7030a73b13498c1825ecaa14dc0a889ee4f1bbf
Facundo Godoy <facu14_23@hotmail.com>
2023-07-02 22:24:28 -0300

    Register auth UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+import ClientOnly from "./components/ClientOnly";
+import RegisterModal from "./components/modals/RegisterModal";
 import NavBar from "./components/navbar/NavBar";
 import "./globals.css";
 import { Nunito } from "next/font/google";
+import ToasterProvider from "./providers/ToasterProvider";
 
 export const metadata = {
   title: "Airbnb",
@@ -15,7 +18,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={font.className}>
-        <NavBar />
+        <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
+          <NavBar />
+        </ClientOnly>
         {children}
       </body>
     </html>
